f88530d1604b8a761b0df5471756488d6262ac22
akenworthy87 <akenworthy87@gmail.com>
2020-08-21 20:28:38 +0000

    Product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import ProductInfo, ProductStock, Category
 
 # Register your models here.
+
+
+class ProductStockInline(admin.TabularInline):
+    model = ProductStock
+
+
+class ProductInfoAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+    inlines = [
+        ProductStockInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(ProductInfo, ProductInfoAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=32)
+    friendly_name = models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class ProductInfo(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    product_discontinued = models.BooleanField(default=False,
+                                               null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductStock(models.Model):
+    product = models.ForeignKey('ProductInfo', null=False, blank=False,
+                                on_delete=models.SET_NULL)
+    variety_friendly = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock_qty = models.IntegerField()
+    stock_reserved = models.IntegerField()
+    variety_discontinued = models.BooleanField(default=False, null=True,
+                                               blank=True)
+
+    def __str__(self):
+        return self.name
