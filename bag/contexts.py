7f3a5e5e61f5c20954fa641c92e0f11e1433735d
akenworthy87 <akenworthy87@gmail.com>
2020-09-07 15:34:44 +0000

    Removed legacy code from bag context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import ProductInfo, ProductStock
+from products.models import ProductStock
 
 
 def bag_contents(request):
@@ -10,31 +10,18 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-    print(bag.items())
     for item_id, item_data in bag.items():
-        if isinstance(item_data, int):
-            print("Is Int")
-            # product = get_object_or_404(ProductInfo, pk=item_id)
-            # total += item_data * product.price
-            # product_count += item_data
-            # bag_items.append({
-            #     'item_id': item_id,
-            #     'quantity': item_data,
-            #     'product': product,
-            # })
-        else:
-            # product = get_object_or_404(ProductInfo, pk=item_id)
-            for variety_id, quantity in item_data['items_by_variety'].items():
-                line = get_object_or_404(ProductStock, pk=variety_id)
-                total += quantity * line.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': line.product,
-                    'variety': variety_id,
-                    'line': line,
-                })
+        for variety_id, quantity in item_data['items_by_variety'].items():
+            line = get_object_or_404(ProductStock, pk=variety_id)
+            total += quantity * line.price
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': line.product,
+                'variety': variety_id,
+                'line': line,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
