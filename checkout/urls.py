a44a467040c37ac6247a3e9986116b40a2590e94
akenworthy87 <akenworthy87@gmail.com>
2020-09-09 21:33:36 +0000

    Imported checkout app from CI miniproject

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-# from .webhooks import webhook
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
@@ -10,5 +10,5 @@ urlpatterns = [
     path('cache_checkout_data/',
          views.cache_checkout_data,
          name='cache_checkout_data'),
-    # path('wh/', webhook, name='webhook'),
+    path('wh/', webhook, name='webhook'),
 ]
