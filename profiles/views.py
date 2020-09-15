aae927fc988cb3925c77c54b6040cdc1cc11d8b8
akenworthy87 <akenworthy87@gmail.com>
2020-09-15 23:44:50 +0000

    Re-enabled order history on profiles, adjusted to fit new models

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
-# from checkout.models import Order
+from checkout.models import Order
 
 
 @login_required
@@ -23,30 +23,30 @@ def profile(request):
                             'the form is valid.'))
     else:
         form = UserProfileForm(instance=profile)
-    # orders = profile.orders.all()
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        # 'orders': orders,
+        'orders': orders,
         'on_profile_page': True
     }
 
     return render(request, template, context)
 
 
-# def order_history(request, order_number):
-#     order = get_object_or_404(Order, order_number=order_number)
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
 
-#     messages.info(request, (
-#         f'This is a past confirmation for order number {order_number}. '
-#         'A confirmation email was sent on the order date.'
-#     ))
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
 
-#     template = 'checkout/checkout_success.html'
-#     context = {
-#         'order': order,
-#         'from_profile': True,
-#     }
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
 
-#     return render(request, template, context)
+    return render(request, template, context)
