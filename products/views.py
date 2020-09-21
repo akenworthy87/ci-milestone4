c1b76d7754784984614b9b025d74c499854d5dc9
akenworthy87 <akenworthy87@gmail.com>
2020-09-21 20:26:11 +0000

    Redid product list cards to correct some alignment issues

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q, F, Min
+from django.db.models import Q, Min
 from django.db.models.functions import Lower
 
 from .models import ProductInfo, ProductStock, Category
